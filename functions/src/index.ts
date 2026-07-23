7490833285fa7b6b985b90dd202c8cf239541491
Adriel <adriel@example.com>
2026-07-23 16:29:37 -0300

    feat: exporta functions financeiras + rules/indices de comissao/lucro

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,4 +2,7 @@ import * as admin from "firebase-admin";
 
 admin.initializeApp();
 
-export {};
+export { registrarProfitSnapshot } from "./registrarProfitSnapshot";
+export { fecharCompetencia } from "./fecharCompetencia";
+export { onAccrualWrite } from "./onAccrualWrite";
+export { conferirCompetencia } from "./conferirCompetencia";
